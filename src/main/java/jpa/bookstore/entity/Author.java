8a66b0fb45bce9c2e17d06407424a042d158ad05
aghowinem <aghowinem@gmail.com>
2023-04-09 16:29:52 +0300

    Adding some features.

--- src/main/java/jpa/bookstore/entity/Author.java
+++ src/main/java/jpa/bookstore/entity/Author.java
@@ -2,12 +2,20 @@ package jpa.bookstore.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "authors")
+@EntityListeners({AuditingEntityListener.class})
 public class Author {
 
     @Id
@@ -18,6 +26,15 @@ public class Author {
     @JsonManagedReference
     @OneToMany(mappedBy = "author")
     private List<Book> books = new ArrayList<>();
+
+    @CreatedBy
+    private String createdBy;
+    @CreatedDate
+    private LocalDate createDate;
+    @LastModifiedBy
+    private String lastModifiedBy;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
     public void addBook(Book book){
         books.add(book);
@@ -38,6 +55,22 @@ public class Author {
         this.books = books;
     }
 
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
     public int getId() {
         return id;
     }
@@ -48,5 +81,21 @@ public class Author {
 
     public List<Book> getBooks() {
         return books;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
     }
 }
--- src/main/java/jpa/bookstore/entity/Book.java
+++ src/main/java/jpa/bookstore/entity/Book.java
@@ -2,9 +2,19 @@ package jpa.bookstore.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
+import org.hibernate.annotations.Formula;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @NamedEntityGraph(name = "loadAuthor", attributeNodes = @NamedAttributeNode("author"))
 @Entity
+@EntityListeners({AuditingEntityListener.class})
 @Table(name = "books")
 public class Book {
 
@@ -18,10 +28,22 @@ public class Book {
     @Transient
     private double discounted;
 
+    @Formula("(select count(*) from books book where book.author_id = id)")
+    private int bookCount;
+
     @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "author_id")
     private Author author;
+
+    @CreatedBy
+    private String createdBy;
+    @CreatedDate
+    private LocalDate createDate;
+    @LastModifiedBy
+    private String lastModifiedBy;
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 
     public void setId(int id) {
         this.id = id;
@@ -43,6 +65,26 @@ public class Book {
         this.discounted = discounted;
     }
 
+    public void setBookCount(int bookCount) {
+        this.bookCount = bookCount;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public void setCreateDate(LocalDate createDate) {
+        this.createDate = createDate;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
     public double getDiscounted() {
         return price*.25;
     }
@@ -62,8 +104,29 @@ public class Book {
     public Author getAuthor() {
         return author;
     }
+
+    public int getBookCount() {
+        return bookCount;
+    }
+
     @PostLoad
     private void calcDiscount(){
         this.setDiscounted(price*.25);
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public LocalDate getCreateDate() {
+        return createDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
     }
 }
